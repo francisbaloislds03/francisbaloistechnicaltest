370a7b46f393fb9bf0b17496be2d07221dbdacf6
francisbaloislds03 <140867932+francisbaloislds03@users.noreply.github.com>
2023-07-31 17:20:33 +1200

    Add files via upload

---
+++ Francis Balois Technical Test/Francis Balois Technical Test/js/main.js
@@ -0,0 +1,117 @@
+//  Cart
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+// Open Cart
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+// Close Cart
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+// Cart Working JS
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+// Making Function
+function ready() {
+  // Reomve Items From Cart
+  var reomveCartButtons = document.getElementsByClassName("cart-remove");
+  console.log(reomveCartButtons);
+  for (var i = 0; i < reomveCartButtons.length; i++) {
+    var button = reomveCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+  // Quantity Changes
+  var quantityInputs = document.getElementsByClassName("cart-quantity");
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+  // Add To Cart
+  var addCart = document.getElementsByClassName("add-cart");
+  for (var i = 0; i < addCart.length; i++) {
+    var button = addCart[i];
+    button.addEventListener("click", addCartClicked);
+  }
+  // Buy Button Work
+  document
+    .getElementsByClassName("btn-buy")[0]
+    .addEventListener("click", buyButtonClicked);
+}
+// Buy Button
+function buyButtonClicked() {
+  alert("Your Order is placed");
+  var cartContent = document.getElementsByClassName("cart-content")[0];
+  while (cartContent.hasChildNodes()) {
+    cartContent.removeChild(cartContent.firstChild);
+  }
+  updatetotal();
+}
+
+// Reomve Items From Cart
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updatetotal();
+}
+// Quantity Changes
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updatetotal();
+}
+// Add To cart
+function addCartClicked(event) {
+  var button = event.target;
+  var shopProducts = button.parentElement;
+  var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+  var price = shopProducts.getElementsByClassName("price")[0].innerText;
+  
+  var price2 = shopProducts.getElementsByClassName("price2")[0].innerText;
+  var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+  addProductToCart(title, price, productImg, price2);
+
+
+
+  updatetotal();
+}
+function addProductToCart(title, price, productImg, price2) {
+  var cartShopBox = document.createElement("div");
+  cartShopBox.classList.add("cart-box");
+  var cartItems = document.getElementsByClassName("cart-content")[0];
+  var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+  var cartItemsNames = cartItems.getElementsByClassName("cart-price2");
+ 
+  for (var i = 0; i < cartItemsNames.length; i++) {
+    if (cartItemsNames[i].innerText > title) {
+     
+      
+    }
+  }
+  var cartBoxContent = `
+                        <img src="${productImg}" alt="" class="cart-img">
+                        <div class="detail-box">
+                            <div class="cart-product-title">${title}</div>
+                            <div class="cart-price">${price}</div>
+                            <div class="cart-price2">${price2}</div>
+                      
+                        </div>
+                        <!-- Remove Cart -->
+                        <i class='bx bxs-trash-alt cart-remove' ></i>`;
+  cartShopBox.innerHTML = cartBoxContent;
+  cartItems.append(cartShopBox);
+  cartShopBox
+    .getElementsByClassName("cart-remove")[0]
+    .addEventListener("click", removeCartItem);
+  cartShopBox
+    .getElementsByClassName("cart-quantity")[0]
+    .addEventListener("change", quantityChanged);
+}
